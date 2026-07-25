3a239883f2832ed12d6af6246cf722973507e075
Paul Levesque <paul-antoine.levesque@lip6.fr>
2026-07-25 14:52:23 -0400

    part 1 : concurrence

--- Distributor/Matrices/MatrixMultiplier.cs
+++ Distributor/Matrices/MatrixMultiplier.cs
@@ -19,7 +19,8 @@ public sealed class MatrixMultiplier : IMatrixMultiplier
         _spanMultiplier = spanMultiplier;
     }
 
-    public Task<Matrix<T>> MultiplyAsync<T>(
+    // décalaration async de la méthode
+    public async Task<Matrix<T>> MultiplyAsync<T>(
         Matrix<T> left,
         Matrix<T> right,
         int spanSize,
@@ -31,19 +32,22 @@ public sealed class MatrixMultiplier : IMatrixMultiplier
         var spans = _splitter.Split(result, spanSize);
         var state = new MatrixMultiplicationState();
 
+	// première correction : création de la liste de tâches
+	var tasks = new List<Task>();
+
         foreach (var resultSpan in spans)
         {
             var leftSpan = new MatrixSpan<T>(left, resultSpan.RowIndex, resultSpan.RowCount, 0, left.ColumnCount);
             var rightSpan = new MatrixSpan<T>(right, 0, right.RowCount, resultSpan.ColumnIndex, resultSpan.ColumnCount);
 
-            lock (state.Lock)
-            {
-                var task = Task.Run(() => _spanMultiplier.MultiplyAsync(leftSpan, rightSpan, resultSpan, state), token);
+	    // deuxième correction : on ajoute la tâche à la liste créée en la lançant et on supprime le bloc lock
+	    tasks.Add(Task.Run(() => _spanMultiplier.MultiplyAsync(leftSpan, rightSpan, resultSpan, state), token));
 
-                task.Wait(token);
-            }
         }
 
-        return Task.FromResult(result);
+	// troiisème correction : on attend que toutes les tâches aient terminé de s'exécuter
+	await Task.WhenAll(tasks);
+	
+        return result;
     }
 }
